5a794ea6e5ea5d2ba955aab10961480514643e3a
Mathew Smith <monkey_man_707@live.com>
2022-12-03 20:24:55 +1300

    Prepare for day 4

---
+++ src/Day04.kt
@@ -0,0 +1,23 @@
+fun main() {
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day04_test")
+    println(part1(testInput))
+    check(part1(testInput) == 24_000)
+    println(part2(testInput))
+//    check(part2(testInput) == 45_000)
+
+    println("---")
+    val input = readInput("Day04_input")
+    println(part1(input))
+    println(part2(input))
+}
+
+
+private fun part1(input: List<String>): Int {
+    return input.size
+}
+
+
+private fun part2(input: List<String>): Int {
+    return input.size
+}

a34e002e943af8d624f19b4c6e054188061d0cf2
Mathew Smith <monkey_man_707@live.com>
2022-12-03 20:23:43 +1300

    Completed day 3

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,10 +1,12 @@
+import kotlin.streams.toList
+
 fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
     println(part1(testInput))
-    check(part1(testInput) == 24_000)
+    check(part1(testInput) == 157)
     println(part2(testInput))
-//    check(part2(testInput) == 45_000)
+    check(part2(testInput) == 70)
 
     println("---")
     val input = readInput("Day03_input")
@@ -12,12 +14,32 @@ fun main() {
     println(part2(input))
 }
 
+private const val points = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 private fun part1(input: List<String>): Int {
-    return input.size
+    val letters = input.map {
+        val firstHalf = it.substring(0, it.length/2)
+        val secondHalf = it.substring(it.length/2, it.length)
+        val commonLetter = firstHalf.chars().toList().intersect(secondHalf.chars().toList())
+        commonLetter.first().toChar()
+    }
+    val scores = letters.map { letter ->
+        points.indexOf(letter) + 1
+    }
+    return scores.sum()
 }
 
 
 private fun part2(input: List<String>): Int {
-    return input.size
+    val groups = input.chunked(3)
+    val badges = groups.map { (one, two, three) ->
+        val oneList = one.chars().toList()
+        val twoList = two.chars().toList()
+        val threeList = three.chars().toList()
+        oneList.intersect(twoList).intersect(threeList).first().toChar()
+    }
+    val scores = badges.map { letter ->
+        points.indexOf(letter) + 1
+    }
+    return scores.sum()
 }

17f569bc30cb8f0c6a55d9afe048c1d029f2a07b
Mathew Smith <monkey_man_707@live.com>
2022-12-04 21:36:00 +1300

    Completed day 4

--- src/Day04.kt
+++ src/Day04.kt
@@ -2,9 +2,9 @@ fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = readInputLines("Day04_test")
     println(part1(testInput))
-    check(part1(testInput) == 24_000)
+    check(part1(testInput) == 2)
     println(part2(testInput))
-//    check(part2(testInput) == 45_000)
+    check(part2(testInput) == 4)
 
     println("---")
     val input = readInputLines("Day04_input")
@@ -14,10 +14,22 @@ fun main() {
 
 
 private fun part1(input: List<String>): Int {
-    return input.size
+    val pairContains = input.map { elfPair ->
+        val (firstElf, secondElf) = elfPair.split(',')
+        val (firstStart, firstEnd) = firstElf.split('-').map { it.toInt() }
+        val (secondStart, secondEnd) = secondElf.split('-').map { it.toInt() }
+        (firstStart <= secondStart && firstEnd >= secondEnd) || (secondStart <= firstStart && secondEnd >= firstEnd)
+    }
+    return pairContains.count { it }
 }
 
 
 private fun part2(input: List<String>): Int {
-    return input.size
+    val pairContains = input.map { elfPair ->
+        val (firstElf, secondElf) = elfPair.split(',')
+        val (firstStart, firstEnd) = firstElf.split('-').map { it.toInt() }
+        val (secondStart, secondEnd) = secondElf.split('-').map { it.toInt() }
+        secondStart in firstStart..firstEnd || secondEnd in firstStart .. firstEnd || firstStart in secondStart..secondEnd || firstEnd in secondStart .. secondEnd
+    }
+    return pairContains.count { it }
 }

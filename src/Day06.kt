955a7defb9ccc8fd8315eab85ce2eced0fcad1e5
Mathew Smith <monkey_man_707@live.com>
2022-12-06 18:23:45 +1300

    Completed day 6

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,23 +1,32 @@
 fun main() {
     // test if implementation meets criteria from the description, like:
-    val testInput = readInputLines("Day06_test")
+    val testInput = readInput("Day06_test")
     println(part1(testInput))
-    check(part1(testInput) == 24_000)
+    check(part1(testInput) == 7)
     println(part2(testInput))
-//    check(part2(testInput) == 45_000)
+    check(part2(testInput) == 19)
 
     println("---")
-    val input = readInputLines("Day06_input")
+    val input = readInput("Day06_input")
     println(part1(input))
     println(part2(input))
 }
 
 
-private fun part1(input: List<String>): Int {
-    return input.size
+private fun part1(input: String): Int {
+    return positionOfFirstUniqueChain(input, 4)
 }
 
 
-private fun part2(input: List<String>): Int {
-    return input.size
+private fun part2(input: String): Int {
+    return positionOfFirstUniqueChain(input, 14)
+}
+
+private fun positionOfFirstUniqueChain(input: String, chainLength: Int): Int {
+    val recentChainChars = MutableList(chainLength) { ' ' }
+    input.forEachIndexed { index, char ->
+        recentChainChars[index % chainLength] = char
+        if (index >= chainLength && recentChainChars.toSet().size == chainLength) return index+1
+    }
+    error("no answer")
 }

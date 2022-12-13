f2e915bcffe59a07cf3175ea5d01b4feada01e90
Mathew Smith <monkey_man_707@live.com>
2022-12-13 17:29:14 +1300

    Prepare for day 13

---
+++ src/Day13.kt
@@ -0,0 +1,25 @@
+fun main() {
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInputLines("Day13_test")
+    val part1 = part1(testInput)
+    println(part1)
+    check(part1 == 24_000)
+    val part2 = part2(testInput)
+    println(part2)
+//    check(part2 == 45_000)
+
+    println("---")
+    val input = readInputLines("Day13_input")
+    println(part1(input))
+    println(part2(input))
+}
+
+
+private fun part1(input: List<String>): Int {
+    return input.size
+}
+
+
+private fun part2(input: List<String>): Int {
+    return input.size
+}

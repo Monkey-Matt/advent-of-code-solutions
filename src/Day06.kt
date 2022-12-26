363f14953745b0d0788d461b1ed93fb4a7b7fdcc
Mathew Smith <monkey_man_707@live.com>
2022-12-26 18:06:36 +1300

    Create alternative solutions for day 6

--- src/Day06.kt
+++ src/Day06.kt
@@ -10,6 +10,8 @@ fun main() {
     val input = readInput("Day06_input")
     println(part1(input))
     println(part2(input))
+
+    testAlternativeSolutions()
 }
 
 
@@ -29,4 +31,30 @@ private fun positionOfFirstUniqueChain(input: String, chainLength: Int): Int {
         if (index >= chainLength && recentChainChars.toSet().size == chainLength) return index+1
     }
     error("no answer")
+}
+
+// ------------------------------------------------------------------------------------------------
+
+private fun testAlternativeSolutions() {
+    val testInput = readInput("Day06_test")
+    check(part1AlternativeSolution(testInput) == 7)
+    check(part2AlternativeSolution(testInput) == 19)
+
+    println("Alternative Solutions:")
+    val input = readInput("Day06_input")
+    println(part1AlternativeSolution(input))
+    println(part2AlternativeSolution(input))
+}
+
+private fun part1AlternativeSolution(input: String): Int {
+    return positionOfFirstUniqueChainAlternative(input, 4)
+}
+
+private fun part2AlternativeSolution(input: String): Int {
+    return positionOfFirstUniqueChainAlternative(input, 14)
+}
+
+private fun positionOfFirstUniqueChainAlternative(input: String, chainLength: Int): Int {
+    val position = input.windowed(chainLength).indexOfFirst { it.toSet().size == chainLength }
+    return position + chainLength
 }
